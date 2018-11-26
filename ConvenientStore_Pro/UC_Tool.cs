7c1059616ffdd1bd1da2281f16577a4cbf5cc3ff
cloudkick195 <cloudkick195@gmail.com>
2018-11-26 15:35:57 +0700

    Source Files

---
+++ ConvenientStore_Pro/UC_Tool.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConvenientStore_Pro
+{
+    public partial class UC_Tool : UserControl
+    {
+        private static UC_Tool _instance;
+        public static UC_Tool Instance
+        {
+            get
+            {
+                if (_instance == null)
+                    _instance = new UC_Tool();
+                return _instance;
+            }
+        }
+        public UC_Tool()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ DAO/DataProvider.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SqlClient;
+namespace DAO
+{
+    public class DataProvider
+    {
+        SqlConnection cn;
+        public DataProvider()
+        {
+            string cnStr = @"Data Source=DESKTOP-9NDS0EG;Initial Catalog=Store;Integrated Security=True";
+            cn = new SqlConnection(cnStr);
+        }
+        public void Connect()
+        {
+            try
+            {
+                if (cn != null && cn.State == System.Data.ConnectionState.Closed)
+                {
+                    cn.Open();
+                }
+            }
+            catch (SqlException ex)
+            {
+                throw ex;
+            }
+        }
+        public void DisConnect()
+        {
+            if (cn != null && cn.State == System.Data.ConnectionState.Open)
+            {
+                cn.Close();
+            }
+        }
+        public int myExcuteScalar(string sql)
+        {
+            SqlCommand CMD = new SqlCommand();
+            CMD.Connection = cn;
+            CMD.CommandText = sql;
+            CMD.CommandType = System.Data.CommandType.Text;
+            Connect();
+            try
+            {
+                int number = (int)CMD.ExecuteScalar();
+                return number;
+            }
+            catch (SqlException ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                DisConnect();
+            }
+        }
+        public SqlDataReader myExcuteReader(string sql)
+        {
+            SqlCommand cmd = new SqlCommand(sql, cn);
+            cmd.CommandType = System.Data.CommandType.Text;
+            try
+            {
+                return (cmd.ExecuteReader());
+            }
+            catch (SqlException ex)
+            {
+
+                throw ex;
+            }
+        }
+        public int myExcuteNonQuery(string sql)
+        {
+            SqlCommand cmd = new SqlCommand(sql, cn);
+            cmd.CommandType = System.Data.CommandType.Text;
+            Connect();
+            try
+            {
+                int numOfRows = cmd.ExecuteNonQuery();
+                return numOfRows;
+            }
+            catch (SqlException ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                DisConnect();
+            }
+        }
+    }
+}
